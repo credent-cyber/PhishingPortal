994f80813bf58096aa6090a3da8dcf83fc18cb87
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2022-08-14 15:01:47 +0530

    CleanupRevert "cleanup"
    This reverts commit afd8bf072600ada5f871511cbb77b9ec561e4ce6.

---
+++ src/PhishingPortal/PhishingPortal.UI.Blazor/wwwroot/scripts/SummerEditorHack.js
@@ -0,0 +1,13 @@
+﻿
+function summernoteEditorHack(code, selector) {
+    let $editor = $(selector);
+    if ($editor !== undefined)
+        $editor.summernote('code', code);
+}
+
+function getSummernoteEditorContent(selector) {
+    let $editor = $(selector);
+
+    if ($editor !== undefined)
+        return $editor.summernote('code');
+}
