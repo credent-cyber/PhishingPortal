d0b62aca33bad0593f9ad32d410fe91d326ba410
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2022-08-14 14:54:20 +0530

    Cleanup

---
+++ src/PhishingPortal/PhishingPortal.UI.Blazor/wwwroot/scripts/SummerEditorHack.js
@@ -0,0 +1,12 @@
+﻿function summernoteEditorHack(code, selector) {
+    let $editor = $(selector);
+    if ($editor !== undefined)
+        $editor.summernote('code', code);
+}
+
+function getSummernoteEditorContent(selector) {
+    let $editor = $(selector);
+
+    if ($editor !== undefined)
+        return $editor.summernote('code');
+}
