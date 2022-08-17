f525f4def91c2924ffcebf42f9483714e42998ac
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2022-08-17 15:06:25 +0530

    Image upload feature added

---
+++ src/PhishingPortal/PhishingPortal.Server/UploadController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Hosting;
+using PhishingPortal.Common;
+
+namespace PhishingPortal.Server
+{
+ 
+		[ApiController]
+		[Route("api/[controller]")] 
+		public class UploadController : ControllerBase
+		{
+			private readonly IWebHostEnvironment env;
+
+			public UploadController(IWebHostEnvironment env)
+			{
+				this.env = env;
+ 
+			}
+
+			[HttpPost]
+			public async Task Post([FromBody] ImageFile[] files)
+			{
+				foreach (var file in files)
+				{
+					var buf = Convert.FromBase64String(file.base64data);
+					//await System.IO.File.WriteAllBytesAsync(env.ContentRootPath + System.IO.Path.DirectorySeparatorChar + file.fileName, buf);
+
+					await System.IO.File.WriteAllBytesAsync("./../PhishingPortal.UI.Blazor/wwwroot/img/email" + System.IO.Path.DirectorySeparatorChar + file.fileName, buf);
+
+					
+				}
+			}
+		}
+	
+}
