65b1a5893fe03635a7fd666306d82710dee733ba
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2022-09-07 23:06:36 +0530

    return url overriding to base url fixed

--- src/PhishingPortal/PhishingPortal.Services.Notification/Email/EmailCampaignProvider.cs
+++ src/PhishingPortal/PhishingPortal.Services.Notification/Email/EmailCampaignProvider.cs
@@ -89,8 +89,8 @@ namespace PhishingPortal.Services.Notification.Email
                 if (template == null)
                     throw new Exception("Template not found");
 
-                if (!string.IsNullOrEmpty(campaign.ReturnUrl))
-                    BaseUrl = campaign.ReturnUrl;
+              
+                  
 
                 var recipients = dbContext.CampaignRecipients.Include(o => o.Recipient).Where(o => o.CampaignId == campaign.Id);
 
