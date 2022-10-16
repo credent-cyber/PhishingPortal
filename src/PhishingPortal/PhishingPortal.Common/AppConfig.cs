28bdc703e874b314e9828ae98b52d61a861b4cd4
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2022-10-16 22:29:33 +0530

    nolayout added for landong page

--- src/PhishingPortal/PhishingPortal.Common/AppConfig.cs
+++ src/PhishingPortal/PhishingPortal.Common/AppConfig.cs
@@ -13,5 +13,6 @@ namespace PhishingPortal.Common
 
         public const int ImportRecipientCsvMinColumns  = 8;
         public const string EmailRegex  = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+        public static readonly string[] restrictEmail = { "gmail", "yahoo", "outlook", "hotmail" };
     }
 }
