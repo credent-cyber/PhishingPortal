469157056727df79c569957f885ca2cad7519d97
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2022-11-02 11:17:31 +0530

    changes in demo requestor

---
+++ src/PhishingPortal/PhishingPortal.UI.Blazor/Client/RequestClient.cs
@@ -0,0 +1,34 @@
+﻿using PhishingPortal.Dto;
+using System.Net.Http.Json;
+
+namespace PhishingPortal.UI.Blazor.Client
+{
+    public class RequestClient : BaseHttpClient
+    {
+        public RequestClient(ILogger<RequestClient> logger, HttpClient httpClient) : base(logger, httpClient)
+        {
+        }
+
+        public async Task<DemoRequestor> CreateDemoRequest(DemoRequestor demo)
+        {
+            DemoRequestor? o;
+            try
+            {
+                var res = await HttpClient.PostAsJsonAsync("api/request/upsert_demorequestor", demo);
+
+                res.EnsureSuccessStatusCode();
+
+                o = await res.Content.ReadFromJsonAsync<DemoRequestor>();
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogCritical(ex, ex.Message);
+                throw;
+            }
+
+            return o ?? demo;
+
+        }
+    }
+}
