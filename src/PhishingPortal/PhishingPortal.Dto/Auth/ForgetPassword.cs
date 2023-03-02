21dcfe15aa8407569d2c13d892220d38cc4e2773
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2023-03-02 17:53:29 +0530

    validation in reset pass

--- src/PhishingPortal/PhishingPortal.Dto/Auth/ForgetPassword.cs
+++ src/PhishingPortal/PhishingPortal.Dto/Auth/ForgetPassword.cs
@@ -5,6 +5,7 @@ namespace PhishingPortal.Dto.Auth
     public class ForgetPasswordRequest
     {
         [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
     }
 }
