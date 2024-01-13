5bea84da6d86e0324fbe994d773f9ca448ed316f
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2024-01-13 13:09:10 +0530

    changes in setting page layout

---
+++ src/PhishingPortal/PhishingPortal.UI.Blazor/wwwroot/scripts/settingPage.js
@@ -0,0 +1,16 @@
+﻿function menu() {
+    let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+        arrow[i].addEventListener("click", (e) => {
+            let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+            arrowParent.classList.toggle("showMenu");
+        });
+    }
+    let sidebar = document.querySelector(".setting_sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
+}
+
