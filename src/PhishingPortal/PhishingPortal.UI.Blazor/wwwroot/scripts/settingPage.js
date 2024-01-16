7beb0f377d74afefe64517b116f5d9080241be7e
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2024-01-16 15:11:56 +0530

    side nav toggle bug fixes

--- src/PhishingPortal/PhishingPortal.UI.Blazor/wwwroot/scripts/settingPage.js
+++ src/PhishingPortal/PhishingPortal.UI.Blazor/wwwroot/scripts/settingPage.js
@@ -1,17 +1,5 @@
-﻿function menu() {
-    let arrow = document.querySelectorAll(".arrow");
-    for (var i = 0; i < arrow.length; i++) {
-        arrow[i].addEventListener("click", (e) => {
-            let arrowParent = e.target.parentElement.parentElement;
-        });
-    }
+﻿
+function menu() {
     let sidebar = document.querySelector(".setting_sidebar");
-    let sidebarBtn = document.querySelector(".bx-menu");
- 
-    sidebarBtn.addEventListener("click", () => {
-        sidebar.classList.toggle("close");
-       
-    });
-
-   
+    sidebar.classList.toggle("close");
 }
