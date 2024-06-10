73d7aeffcb8795c1fc11299f215c6d1090197838
Mohammad Usaid <mohammad.usaid@credentinfotech.com>
2024-06-10 15:06:40 +0530

    Report Recipant Mapping bug fixes

--- src/PhishingPortal/PhishingPortal.DataContext/TenantDbContext.cs
+++ src/PhishingPortal/PhishingPortal.DataContext/TenantDbContext.cs
@@ -45,6 +45,9 @@ namespace PhishingPortal.DataContext
             modelBuilder.Entity<RecipientGroupMapping>().HasKey(o => new { o.GroupId, o.RecipientId });
             modelBuilder.Entity<TrainingLog>().Ignore(o => o.TrainingName);
             modelBuilder.Entity<TrainingLog>().Ignore(o => o.RecipientName);
+            modelBuilder.Entity<CampaignLog>().Ignore(o => o.RecipantEmail);
+            modelBuilder.Entity<CampaignLog>().Ignore(o => o.RecipantMobile);
+            modelBuilder.Entity<CampaignLog>().Ignore(o => o.RecipantDepartment);
 
             base.OnModelCreating(modelBuilder);
 
